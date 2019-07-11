1a4c30116ffa11d79404043bb9fd3fb0c6364aab
Hiếu Nghi <hieunghik25@gmail.com>
2019-07-11 09:17:00 +0700

    commit

--- DemoOdoo/MyProject/models/customer.py
+++ DemoOdoo/MyProject/models/customer.py
@@ -8,11 +8,3 @@ class Customers(models.Model):
     _name = "customers"
     _inherit = "employees"
     _description = u"Danh sách khách hàng"
-
-    @api.model
-    def create(self,vals):
-        vals['code'] = int(self.search_count([]))
-        return super(Customers, self).create(vals)
-
-
-    _sql_constraints = [('ma_nha_cung_cap_duy_nhat', 'UNIQUE(code)', u'Mã trùng rồi điền mã cái khác đi')]
